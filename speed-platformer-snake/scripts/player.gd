4cd72c7f5129d06d618b490beaa037a8b850fade
TheRealRaichu <ilyas.mhd.khan@gmail.com>
2026-03-21 12:42:36 -0500

    scarf invincibility hotfix

--- speed-platformer-snake/scripts/player.gd
+++ speed-platformer-snake/scripts/player.gd
@@ -169,6 +169,7 @@ func use_sugar():
 	current_sugar_timer.timeout.connect(sugar_timeout) # after duration, disable sugar effects
 
 func use_reeler():
+	scarf_invincible = true
 	scarf_invincible_timer()
 	AudioManager.play("usescarfreeler", 3) # play blink restore use noise
 	scarf.reel() # tell scarf to reel back
