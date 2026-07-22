ff1cc6df20e716d68b808e2bc22da89163a847d9
James Zheng <jimmyztx@gmail.com>
2026-07-21 22:51:29 -0500

    Added the Settings button, including variables and a new node to go into. Text of settings is incomplete, need to use the name of "settings"

--- speed-platformer-snake/scripts/mainMenu.gd
+++ speed-platformer-snake/scripts/mainMenu.gd
@@ -6,21 +6,26 @@ var scene_manager
 # reference to the 2 nodes.
 @onready var howtoplay := $HowToPlay
 @onready var mainmenu := $MainMenuTitle
+@onready var settings := $Settings
 
 # references to the cursor images
 @onready var cursor_start := $MainMenuTitle/VBoxContainer/StartNode/StartCursor
 @onready var cursor_htp := $MainMenuTitle/VBoxContainer/HTPNode/HTPCursor
 @onready var cursor_quit := $MainMenuTitle/VBoxContainer/QuitNode/QuitCursor
+@onready var cursor_settings := $MainMenuTitle/VBoxContainer/SettingsNode/SettingsCursor
 
 # refernces to the buttons
 @onready var start_button := $"MainMenuTitle/VBoxContainer/StartNode/start game"
 @onready var howtoplay_button := $"MainMenuTitle/VBoxContainer/HTPNode/howtoplay game"
 @onready var quit_button := $"MainMenuTitle/VBoxContainer/QuitNode/quit game"
 @onready var back_button := $HowToPlay/BackNode/back
+@onready var settings_button := $MainMenuTitle/VBoxContainer/SettingsNode/settings
 
+# references to texts
 @onready var start_text := $"MainMenuTitle/VBoxContainer/StartNode/StartText"
 @onready var htp_text := $"MainMenuTitle/VBoxContainer/HTPNode/HowToPlayText"
 @onready var quit_text := $"MainMenuTitle/VBoxContainer/QuitNode/QuitText"
+@onready var settings_text := $MainMenuTitle/VBoxContainer/SettingsNode/SettingsText
 
 ## Initialize the main menu as true and How To Play section as false when first time running, and initilizaed cursor as false.
 func _ready() -> void:
@@ -28,9 +33,11 @@ func _ready() -> void:
 
 	mainmenu.visible = true
 	howtoplay.visible = false
+	settings.visible = false
 	cursor_start.visible = true
 	cursor_htp.visible = false
 	cursor_quit.visible = false
+	cursor_settings.visible = false
 
 ## Starts the game when pressed
 func _on_start_game_pressed() -> void:
@@ -43,7 +50,6 @@ func _on_start_game_focus_entered() -> void:
 func _on_start_game_focus_exited() -> void:
 	cursor_start.visible = false
 	start_text.unfocused()
-
 	
 
 ## Goes to the How to play section inside of main menu scene through turn on and off nodes.
@@ -59,8 +65,19 @@ func _on_howtoplay_game_focus_exited() -> void:
 func _on_howtoplay_game_focus_entered() -> void:
 	cursor_htp.visible = true
 	htp_text.focused()
-	
-	
+
+## Goes to Settings
+func _on_settings_pressed() -> void:
+	mainmenu.visible = false
+	settings.visible = true
+
+func _on_settings_focus_exited() -> void:
+	cursor_settings.visible = false
+	settings_text.unfocused()
+
+func _on_settings_focus_entered() -> void:
+	cursor_settings.visible = true
+	settings_text.focused()
 
 ## Stops the game when pressed
 func _on_quit_game_pressed() -> void:
