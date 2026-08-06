9a76fe06bcd06fcdf742590873f82f2114026b86
James Zheng <jimmyztx@gmail.com>
2026-08-06 00:08:24 -0500

    Fully put all the scenes as submenus for the main menu. The keybinds currently is not working and the black backdrop is also broken.

--- speed-platformer-snake/scripts/mainMenu.gd
+++ speed-platformer-snake/scripts/mainMenu.gd
@@ -10,6 +10,9 @@ var game_title: TextureRect
 # scene references
 var main_menu: VBoxContainer
 var game_modes: VBoxContainer
+var how_to_play_menu: VBoxContainer
+var settings_menu: VBoxContainer
+var keybind_menu: VBoxContainer
 
 ## MAIN MENU
 
@@ -36,23 +39,63 @@ var settings_text: TextureRect
 var cursor_normal: Control
 var cursor_practice: Control
 var cursor_two_player: Control
-var cursor_settings_back: Control
+var cursor_game_back: Control
 
 # buttons
 var normal_button: Button
 var practice_button: Button
 var two_player_button: Button
-var settings_back_button: Button
+var game_back_button: Button
 
 # texts
 var normal_text: TextureRect
 var practice_text: TextureRect
 var two_player_text: TextureRect
 
+## HOW TO PLAY
+# buttons
+var tutorial_button: Button
+var how_to_play_back_button: Button
+
+# cursor images
+var tutorial_cursor: Control
+var how_to_play_back_cursor: Control
+
+# texts
+var tutorial_text: TextureRect
+var how_to_play_back_text: TextureRect
+var basics_header: Label
+var basics_body: Label
+
+## SETTINGS
+# controls
+var volume_decrease_button: Button
+var volume_increase_button: Button
+var fullscreen_check: CheckBox
+var keybind_button: Button
+var settings_back_button: Button
+
+# cursor images
+var cursor_audio: Control
+var cursor_fullscreen: Control
+var cursor_keybind: Control
+var cursor_settings_back_menu: Control
+
+# texts
+var audio_text: TextureRect
+var volume_value_text: TextureRect
+var fullscreen_text: TextureRect
+var keybind_text: TextureRect
+var settings_back_text: TextureRect
+
+const VOLUME_STEP_DB := 5.0
+
 ## Initialize the main menu panels and wire their controls.
 func _ready() -> void:
 	_cache_scene_references()
 	_connect_main_menu_signals()
+	_connect_how_to_play_signals()
+	_connect_settings_signals()
 	_setup_initial_state()
 
 # Link References to Different Scenes
@@ -60,6 +103,9 @@ func _cache_scene_references() -> void:
 	main_screen = get_node("Main Screen")
 	main_menu = get_node("Main")
 	game_modes = get_node("GameModes")
+	how_to_play_menu = get_node("HowToPlay")
+	settings_menu = get_node("Settings")
+	keybind_menu = get_node("Keybind")
 
 # Link references to the variables
 func _connect_main_menu_signals() -> void:
@@ -106,19 +152,98 @@ func _connect_main_menu_signals() -> void:
 
 	_connect_game_mode_signals()
 
+func _connect_how_to_play_signals() -> void:
+	# references to buttons
+	tutorial_button = get_node("HowToPlay/TutorialPlayNode/tutorial")
+	how_to_play_back_button = get_node("HowToPlay/BackNode/back")
+
+	# references to cursors
+	tutorial_cursor = get_node("HowToPlay/TutorialPlayNode/PlayCursor")
+	how_to_play_back_cursor = get_node("HowToPlay/BackNode/BackCursor")
+
+	# references to text
+	tutorial_text = get_node("HowToPlay/TutorialPlayNode/PlayTutorialText")
+	how_to_play_back_text = get_node("HowToPlay/BackNode/BackText")
+	basics_header = get_node("THE BASICS header")
+	basics_body = get_node("THE BASICS body")
+
+	# signal hookups
+	tutorial_button.focus_entered.connect(_on_tutorial_focus_entered)
+	tutorial_button.focus_exited.connect(_on_tutorial_focus_exited)
+	tutorial_button.pressed.connect(_on_tutorial_pressed)
+	how_to_play_back_button.focus_entered.connect(_on_how_to_play_back_focus_entered)
+	how_to_play_back_button.focus_exited.connect(_on_how_to_play_back_focus_exited)
+	how_to_play_back_button.pressed.connect(_on_how_to_play_back_pressed)
+
+func _connect_settings_signals() -> void:
+	# references to controls
+	volume_decrease_button = get_node("Settings/Audio/VolumeDown")
+	volume_increase_button = get_node("Settings/Audio/VolumeUp")
+	fullscreen_check = get_node("Settings/Fullscreen/FullScreenCheck")
+	keybind_button = get_node("Settings/Keybinds/keybind")
+	settings_back_button = get_node("Settings/BackNode/back")
+
+	# references to cursors
+	cursor_audio = get_node("Settings/Audio/AudioCursor")
+	cursor_fullscreen = get_node("Settings/Fullscreen/FullScreenCursor")
+	cursor_keybind = get_node("Settings/Keybinds/KeybindCursor")
+	cursor_settings_back_menu = get_node("Settings/BackNode/BackCursor")
+
+	# references to text
+	audio_text = get_node("Settings/Audio/AudioText")
+	volume_value_text = get_node("Settings/Audio/VolumeValue")
+	fullscreen_text = get_node("Settings/Fullscreen/FullScreenCheckText")
+	keybind_text = get_node("Settings/Keybinds/KeybindText")
+	settings_back_text = get_node("Settings/BackNode/BackText")
+
+	# audio control signals
+	volume_decrease_button.focus_entered.connect(_on_audio_focus_entered)
+	volume_decrease_button.focus_exited.connect(_on_audio_focus_exited)
+	volume_decrease_button.pressed.connect(_on_volume_decrease_pressed)
+	volume_increase_button.focus_entered.connect(_on_audio_focus_entered)
+	volume_increase_button.focus_exited.connect(_on_audio_focus_exited)
+	volume_increase_button.pressed.connect(_on_volume_increase_pressed)
+
+	# fullscreen control signals
+	fullscreen_check.focus_entered.connect(_on_fullscreen_focus_entered)
+	fullscreen_check.focus_exited.connect(_on_fullscreen_focus_exited)
+	fullscreen_check.toggled.connect(_on_fullscreen_toggled)
+
+	# keybind control signals
+	keybind_button.focus_entered.connect(_on_keybind_focus_entered)
+	keybind_button.focus_exited.connect(_on_keybind_focus_exited)
+	keybind_button.pressed.connect(_on_keybind_pressed)
+
+	# back button signals
+	settings_back_button.focus_entered.connect(_on_settings_back_menu_focus_entered)
+	settings_back_button.focus_exited.connect(_on_settings_back_menu_focus_exited)
+	settings_back_button.pressed.connect(_on_settings_back_pressed)
+
+	_initialize_settings_controls()
+
+func _initialize_settings_controls() -> void:
+	if fullscreen_check:
+		fullscreen_check.set_pressed_no_signal(DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN)
+
+	_update_volume_text()
+
+func _set_how_to_play_text_visible(visible: bool) -> void:
+	basics_header.visible = visible
+	basics_body.visible = visible
+
 # Link references to the game mode variables
 func _connect_game_mode_signals() -> void:
 	# references to cursors
 	cursor_normal = get_node("GameModes/NormalMode/NormalCursor")
 	cursor_practice = get_node("GameModes/PracticeMode/PracticeCursor")
 	cursor_two_player = get_node("GameModes/2PlayerMode/2PlayerCursor")
-	cursor_settings_back = get_node("GameModes/Back/BackCursor")
+	cursor_game_back = get_node("GameModes/Back/BackCursor")
 
 	# references to buttons
 	normal_button = get_node("GameModes/NormalMode/normal")
 	practice_button = get_node("GameModes/PracticeMode/practice")
 	two_player_button = get_node("GameModes/2PlayerMode/2player")
-	settings_back_button = get_node("GameModes/Back/back")
+	game_back_button = get_node("GameModes/Back/back")
 
 	# references to text
 	normal_text = get_node("GameModes/NormalMode/NormalText")
@@ -141,18 +266,52 @@ func _connect_game_mode_signals() -> void:
 	two_player_button.pressed.connect(_on_two_player_pressed)
 
 	# references to back
-	settings_back_button.focus_entered.connect(_on_back_focus_entered)
-	settings_back_button.focus_exited.connect(_on_back_focus_exited)
-	settings_back_button.pressed.connect(_on_back_pressed)
+	game_back_button.focus_entered.connect(_on_back_focus_entered)
+	game_back_button.focus_exited.connect(_on_back_focus_exited)
+	game_back_button.pressed.connect(_on_back_pressed)
 
 func _set_main_menu_visible(visible: bool) -> void:
 	main_menu.visible = visible
 	game_title.visible = visible
+	settings_menu.visible = false
+	keybind_menu.visible = false
+	_set_how_to_play_text_visible(false)
 
 func _set_game_modes_visible(visible: bool) -> void:
 	game_modes.visible = visible
 	main_menu.visible = not visible
 	game_title.visible = not visible
+	how_to_play_menu.visible = false
+	settings_menu.visible = false
+	keybind_menu.visible = false
+	_set_how_to_play_text_visible(false)
+
+func _set_how_to_play_visible(visible: bool) -> void:
+	how_to_play_menu.visible = visible
+	main_menu.visible = not visible
+	game_modes.visible = false
+	game_title.visible = not visible
+	settings_menu.visible = false
+	keybind_menu.visible = false
+	_set_how_to_play_text_visible(visible)
+
+func _set_settings_visible(visible: bool) -> void:
+	settings_menu.visible = visible
+	main_menu.visible = not visible
+	game_modes.visible = false
+	how_to_play_menu.visible = false
+	keybind_menu.visible = false
+	game_title.visible = not visible
+	_set_how_to_play_text_visible(false)
+
+func _set_keybind_visible(visible: bool) -> void:
+	keybind_menu.visible = visible
+	settings_menu.visible = not visible
+	main_menu.visible = false
+	game_modes.visible = false
+	how_to_play_menu.visible = false
+	game_title.visible = false
+	_set_how_to_play_text_visible(false)
 
 func _hide_all_cursors() -> void:
 	cursor_start.visible = false
@@ -162,13 +321,23 @@ func _hide_all_cursors() -> void:
 	cursor_normal.visible = false
 	cursor_practice.visible = false
 	cursor_two_player.visible = false
-	cursor_settings_back.visible = false
+	cursor_game_back.visible = false
+	tutorial_cursor.visible = false
+	how_to_play_back_cursor.visible = false
+	cursor_audio.visible = false
+	cursor_fullscreen.visible = false
+	cursor_keybind.visible = false
+	cursor_settings_back_menu.visible = false
 
 # Set Visibilities of the cursors
 func _setup_initial_state() -> void:
 	# Set the vboxcontainers visibility
 	_set_main_menu_visible(true)
 	game_modes.visible = false
+	how_to_play_menu.visible = false
+	settings_menu.visible = false
+	keybind_menu.visible = false
+	_set_how_to_play_text_visible(false)
 
 	# Main Menu
 	_hide_all_cursors()
@@ -179,7 +348,7 @@ func _setup_initial_state() -> void:
 	cursor_normal.visible = false
 	cursor_practice.visible = false
 	cursor_two_player.visible = false
-	cursor_settings_back.visible = false
+	cursor_game_back.visible = false
 
 ## Opens the game modes panel when pressed
 func _on_start_game_pressed() -> void:
@@ -199,7 +368,10 @@ func _on_start_game_focus_exited() -> void:
 
 ## Goes to the How to play section inside of main menu scene through panel switching.
 func _on_howtoplay_game_pressed() -> void:
-	scene_manager.how_to_play()
+	_set_how_to_play_visible(true)
+	_hide_all_cursors()
+	how_to_play_back_button.grab_focus()
+	how_to_play_back_cursor.visible = true
 
 func _on_howtoplay_game_focus_exited() -> void:
 	cursor_htp.visible = false
@@ -211,7 +383,11 @@ func _on_howtoplay_game_focus_entered() -> void:
 
 ## Goes to Settings
 func _on_settings_pressed() -> void:
-	scene_manager.settings()
+	_set_settings_visible(true)
+	_hide_all_cursors()
+	volume_decrease_button.grab_focus()
+	cursor_audio.visible = true
+	audio_text.focused()
 
 func _on_settings_focus_exited() -> void:
 	cursor_settings.visible = false
@@ -220,6 +396,66 @@ func _on_settings_focus_exited() -> void:
 func _on_settings_focus_entered() -> void:
 	cursor_settings.visible = true
 	settings_text.focused()
+
+func _on_audio_focus_entered() -> void:
+	cursor_audio.visible = true
+	audio_text.focused()
+
+func _on_audio_focus_exited() -> void:
+	cursor_audio.visible = false
+	audio_text.unfocused()
+
+func _on_fullscreen_focus_entered() -> void:
+	cursor_fullscreen.visible = true
+	fullscreen_text.focused()
+
+func _on_fullscreen_focus_exited() -> void:
+	cursor_fullscreen.visible = false
+	fullscreen_text.unfocused()
+
+func _on_keybind_focus_entered() -> void:
+	cursor_keybind.visible = true
+	keybind_text.focused()
+
+func _on_keybind_focus_exited() -> void:
+	cursor_keybind.visible = false
+	keybind_text.unfocused()
+
+func _on_volume_decrease_pressed() -> void:
+	_adjust_music_volume(-VOLUME_STEP_DB)
+
+func _on_volume_increase_pressed() -> void:
+	_adjust_music_volume(VOLUME_STEP_DB)
+
+func _adjust_music_volume(delta_db: float) -> void:
+	var current_volume := AudioManager.get_bus_volume_db(AudioManager.MUSIC_BUS)
+	AudioManager.set_bus_volume_db(AudioManager.MUSIC_BUS, current_volume + delta_db)
+	_update_volume_text()
+
+func _update_volume_text() -> void:
+	if volume_value_text:
+		volume_value_text.visible = true
+
+func _on_fullscreen_toggled(pressed: bool) -> void:
+	var target_mode := DisplayServer.WINDOW_MODE_FULLSCREEN if pressed else DisplayServer.WINDOW_MODE_WINDOWED
+	if DisplayServer.window_get_mode() != target_mode:
+		DisplayServer.window_set_mode(target_mode)
+
+func _on_keybind_pressed() -> void:
+	_set_keybind_visible(true)
+
+func _on_settings_back_menu_focus_entered() -> void:
+	cursor_settings_back_menu.visible = true
+	settings_back_text.focused()
+
+func _on_settings_back_menu_focus_exited() -> void:
+	cursor_settings_back_menu.visible = false
+	settings_back_text.unfocused()
+
+func _on_settings_back_pressed() -> void:
+	_set_settings_visible(false)
+	_hide_all_cursors()
+	start_button.grab_focus()
 
 ## Stops the game when pressed
 func _on_quit_game_pressed() -> void:
@@ -278,7 +514,39 @@ func _on_back_pressed() -> void:
 	start_button.grab_focus()
 
 func _on_back_focus_entered() -> void:
-	cursor_settings_back.visible = true
+	cursor_game_back.visible = true
 
 func _on_back_focus_exited() -> void:
-	cursor_settings_back.visible = false
+	cursor_game_back.visible = false
+
+## HOW TO PLAY
+func _on_tutorial_pressed() -> void:
+	scene_manager.start_tutorial()
+
+func _on_tutorial_focus_entered() -> void:
+	tutorial_cursor.visible = true
+	tutorial_text.focused()
+
+func _on_tutorial_focus_exited() -> void:
+	tutorial_cursor.visible = false
+	tutorial_text.unfocused()
+
+func _on_how_to_play_back_pressed() -> void:
+	_set_how_to_play_visible(false)
+	_hide_all_cursors()
+	start_button.grab_focus()
+
+func _on_how_to_play_back_focus_entered() -> void:
+	how_to_play_back_cursor.visible = true
+	how_to_play_back_text.focused()
+
+func _on_how_to_play_back_focus_exited() -> void:
+	how_to_play_back_cursor.visible = false
+	how_to_play_back_text.unfocused()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if keybind_menu.visible and event.is_action_pressed("ui_cancel"):
+		_set_keybind_visible(false)
+		game_back_button.grab_focus()
+		get_viewport().set_input_as_handled()
+		return
