2dea040b740f2df43673d88382bf212afab5800c
TheRealRaichu <140770946+TheRealRaichu@users.noreply.github.com>
2026-09-15 20:14:10 -0500

    leaderboard is now accessible from main menu

--- speed-platformer-snake/scripts/mainMenu.gd
+++ speed-platformer-snake/scripts/mainMenu.gd
@@ -7,10 +7,12 @@ var scene_manager
 # refernces to the buttons
 @export var start_button : UIButton
 @export var howtoplay_button : UIButton
+@export var leaderboard_button : UIButton
 @export var quit_button : UIButton
 
 @export var menu_buttons : VBoxContainer
 
+@export var leaderboard_scene : PackedScene
 
 ## Initialize the main menu as true and How To Play section as false when first time running, and initilizaed cursor as false.
 func _ready() -> void:
@@ -22,8 +24,19 @@ func _on_start_pressed() -> void:
 
 ## Goes to the How to play section inside of main menu scene through turn on and off nodes.
 func _on_how_to_play_pressed() -> void:
-	pass
 	scene_manager.how_to_play()
+
+## opens the leaderboard
+func _on_leaderboard_pressed() -> void:
+	var leaderboard_inst = leaderboard_scene.instantiate() as LeaderboardUI
+	leaderboard_inst.closed.connect(leaderboard_closed)
+	enbacken(true)
+	# add to scene manager for modulate and input indpendence!
+	add_sibling(leaderboard_inst)
+
+func leaderboard_closed():
+	enbacken(false)
+	leaderboard_button.grab_focus()
 
 ## Stops the game when pressed
 func _on_quit_pressed() -> void:
